270879d2adce5704b0decfb26c39768b33c27634
J22T <jordan22taylor@gmail.com>
2022-11-23 22:02:21 -0800

    Dynamic API

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,19 @@
+import { EventNote } from '@mui/icons-material';
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
 
 const options = {
-params: {
-  maxResults: 50,
-},
+  params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
 
 export const fetchFromAPI = async (url) => {
- const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+const { data } = await axios.get(`${BASE_URL}/${url}`,
+ options);
 
  return data;
-};
+}
