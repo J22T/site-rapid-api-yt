ba90d8fb01da40c51d51834adcacc88b45197f01
J22T <jordan22taylor@gmail.com>
2022-11-28 14:16:20 -0800

    fetchFromAPI

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,21 @@
-import { EventNote } from '@mui/icons-material';
+import { data } from 'autoprefixer';
 import axios from 'axios';
 
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
 
 const options = {
-  params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+  url: 'BASE_URL',
+  params: { },
   headers: {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
 
-export const fetchFromAPI = async (url) => {
-const { data } = await axios.get(`${BASE_URL}/${url}`,
- options);
+/baseUrl/getChannel
 
- return data;
+export const fetchFromAPI = async (url) => {
+const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 }
+
+return data;
